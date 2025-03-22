144828f0f88aad5b3472f2afc8bea7307211216e
AllanCruz9601 <acruzp8@miumg.edu.gt>
2025-03-21 23:07:17 -0600

    Se agrega el proyecto

--- src/main/java/umg/edu/gt/desarrollo/estructuradedatos2025/ejercicios/EjercicioLinkedList.java
+++ src/main/java/umg/edu/gt/desarrollo/estructuradedatos2025/ejercicios/EjercicioLinkedList.java
@@ -1,16 +1,62 @@
 package umg.edu.gt.desarrollo.estructuradedatos2025.ejercicios;
 
+import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class EjercicioLinkedList {
-	
-	/** INSTRUCCIONES
- 	Escriba el algoritmo que resuelve el problema en esta clase.
-	Debe crear un package llamado umg.edu.gt.test.EjercicioLinkedList que corresponda al Test de esta clase.
-	Genere un Test por cada ejemplo y fuerce que uno de esos Test falle, puede implementar retornar un resultado
-	y compararlo con el esperado.
-	 */
-	
-	// Usando LinkedList de Java Collections, resuelva los siguientes problemas
-	// Problema 1: Dada una LinkedList<Integer>, escribir un método que elimine los valores duplicados, dejando solo la primera aparición de cada número.
-	// Problema 2: Implementar un método que invierta los elementos de una LinkedList<String> sin usar otra lista o ArrayList.
-	// Problema 3: Dadas dos listas enlazadas ordenadas de enteros, escribir un método que devuelva una nueva LinkedList<Integer> con los elementos de ambas listas intercalados en orden.
+
+    // Problema 1: Eliminar duplicados en una LinkedList<Integer>
+    public static void eliminarDuplicados(LinkedList<Integer> lista) {
+        HashSet<Integer> seen = new HashSet<>();
+        Iterator<Integer> iter = lista.iterator();
+        
+        while (iter.hasNext()) {
+            Integer num = iter.next();
+            if (seen.contains(num)) {
+                iter.remove();
+            } else {
+                seen.add(num);
+            }
+        }
+    }
+
+    // Problema 2: Invertir una LinkedList<String> sin usar otra lista
+    public static void invertirLista(LinkedList<String> lista) {
+        int n = lista.size();
+        for (int i = 0; i < n / 2; i++) {
+            String temp = lista.get(i);
+            lista.set(i, lista.get(n - 1 - i));
+            lista.set(n - 1 - i, temp);
+        }
+    }
+
+    // Problema 3: Intercalar dos listas ordenadas en una nueva LinkedList<Integer>
+    public static LinkedList<Integer> intercalarListas(LinkedList<Integer> lista1, LinkedList<Integer> lista2) {
+        LinkedList<Integer> resultado = new LinkedList<>();
+        int i = 0, j = 0;
+
+        while (i < lista1.size() && j < lista2.size()) {
+            if (lista1.get(i) <= lista2.get(j)) {
+                resultado.add(lista1.get(i));
+                i++;
+            } else {
+                resultado.add(lista2.get(j));
+                j++;
+            }
+        }
+
+        // Agregar los elementos restantes
+        while (i < lista1.size()) {
+            resultado.add(lista1.get(i));
+            i++;
+        }
+
+        while (j < lista2.size()) {
+            resultado.add(lista2.get(j));
+            j++;
+        }
+
+        return resultado;
+    }
 }
